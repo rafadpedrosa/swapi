ad62cf76356c69c4437d48081a9a25be06a6985d
Rafael Dias Pedrosa <rafadpedrosa@gmail.com>
2019-12-19 19:55:34 -0300

    Add cors policy

--- server.js
+++ server.js
@@ -10,27 +10,29 @@ const rp = require('request-promise');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const port = process.env.PORT || 8088;
+const port = process.env.PORT || 8888;
+var cors = require('cors')
 
 //---------------------------------------
 // set the view engine to ejs
 //---------------------------------------
 app.set('view engine', 'ejs');
 
+app.use(cors())
 
 //---------------------------------------
 // compare and sort API responses
 //---------------------------------------
 function sortStrings(a, b) {
-  // Assuming you want case-insensitive comparison
-  a = a.toLowerCase();
-  b = b.toLowerCase();
-  return (a < b) ? -1 : (a > b) ? 1 : 0;
+	// Assuming you want case-insensitive comparison
+	a = a.toLowerCase();
+	b = b.toLowerCase();
+	return (a < b) ? -1 : (a > b) ? 1 : 0;
 }
 // https://stackoverflow.com/questions/19259233/sorting-json-by-specific-element-alphabetically
 
 function sortNumber(a,b) {
-    return a - b;
+	return a - b;
 }
 //https://stackoverflow.com/questions/1063007/how-to-sort-an-array-of-integers-correctly
 
@@ -41,50 +43,48 @@ function sortNumber(a,b) {
 // create route for index page
 //---------------------------------------
 app.get('/', function(req, res) {
-    let tagline = "Learn more about the Star Wars universe";
+	let tagline = "Learn more about the Star Wars universe";
 	res.render('pages/index', {
 		tagline: tagline
 	});
 });
 
- 
+
 //-----------------------------------------------
 // create route for characters page - limit to 50
 //-----------------------------------------------
-app.get('/characters', (req, res) => {
+app.get('/people', (req, res) => {
 	console.log(req.query.sort);
 	//https://stackoverflow.com/questions/6912584/how-to-get-get-query-string-variables-in-express-js-on-node-js
 	rp({
 		uri: 'https://swapi.co/api/people/',
-	  	json: true
+		json: true
 	})
-	.then((data) => {
-		let parsedQuery = req.query.sort;
-		let results = data.results;
-		if (parsedQuery === 'name') {
-			results.sort(function(a, b) {
-			  return sortStrings(a.name, b.name);
-			})
-		}
-		if (parsedQuery === 'mass') {
-			results.sort(function(a, b) {
-			  return sortNumber(a.mass, b.mass);
-			})
-		}
-		if (parsedQuery === 'height') {
-			results.sort(function(a, b) {
-			  return sortNumber(a.height, b.height);
-			})
-		}
-		res.render('pages/characters', {
-			data: results
-		});
-		sortByName = false;
-	})
-	.catch((err) => {
-		// Deal with the error
-		console.log(err);
-	})
+		.then((data) => {
+			let parsedQuery = req.query.sort;
+			let results = data.results;
+			if (parsedQuery === 'name') {
+				results.sort(function(a, b) {
+					return sortStrings(a.name, b.name);
+				})
+			}
+			if (parsedQuery === 'mass') {
+				results.sort(function(a, b) {
+					return sortNumber(a.mass, b.mass);
+				})
+			}
+			if (parsedQuery === 'height') {
+				results.sort(function(a, b) {
+					return sortNumber(a.height, b.height);
+				})
+			}
+			res.json(results);
+			// sortByName = false;
+		})
+		.catch((err) => {
+			// Deal with the error
+			console.log(err);
+		})
 });
 
 
@@ -96,17 +96,17 @@ app.get('/characters', (req, res) => {
 app.get('/character/:name', (req, res) => {
 	rp({
 		uri: `https://swapi.co/api/people/?search=${req.params.name}`,
-	  	json: true
+		json: true
 	})
-	.then((data) => {
-		res.render('pages/character', {
-			data: data.results
-		});
-	})
-	.catch((err) => {
-		// Deal with the error
-		console.log(err);
-	})
+		.then((data) => {
+			res.render('pages/character', {
+				data: data.results
+			});
+		})
+		.catch((err) => {
+			// Deal with the error
+			console.log(err);
+		})
 });
 
 
@@ -118,7 +118,7 @@ function extractNames(param) {
 		param.map(residentUri => {
 			return rp({
 				uri: residentUri,
-			  	json: true
+				json: true
 			})
 		})
 	).then(residents => {
@@ -142,9 +142,9 @@ function getPlanets(planets) {
 				})
 		})
 	)
-	.then(() => {
-		return result;
-	})
+		.then(() => {
+			return result;
+		})
 }
 
 //-------------------------------------------------------
@@ -153,32 +153,30 @@ function getPlanets(planets) {
 app.get('/planetresidents', (req, res, next)  => {
 	rp({
 		uri: 'http://swapi.co/api/planets/',
-	  	json: true
+		json: true
 	})
-	.then((data) => {
-		let planets = data.results;
-		getPlanets(planets)
-		.then(p => {
-			res.render('pages/planetresidents', {
-				data: JSON.stringify(p)
-			});
-		});
-		
-	})
-	.catch((err) => {
-		console.log(err);
-	})
+		.then((data) => {
+			let planets = data.results;
+			getPlanets(planets)
+				.then(p => {
+					res.render('pages/planetresidents', {
+						data: JSON.stringify(p)
+					});
+				});
+
+		})
+		.catch((err) => {
+			console.log(err);
+		})
 });
 
 
 //---------------------------------------
 // set up port listener
 //---------------------------------------
-server.listen(port,'0.0.0.0',function(){
- server.close(function(){
-   server.listen(port,'0.0.0.0')
- })
+app.listen(port, function () {
+	console.log('CORS-enabled web server listening on port 80')
 })
-console.log('8088 is the magic port');
+console.log('8888 is the magic port');
 
 
